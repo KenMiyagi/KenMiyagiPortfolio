0856e032d0574e7329ff8c8f68c0272dccc4cbf1
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-08 00:52:39 -0300

    Initialize back & front

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const server = require("./app");
+const { conn } = require("./db");
+const PORT = process.env.PORT || 3001;
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`%s listening on PORT ${PORT}`);
+  });
+});
+
