343478923419ec9d7f366ac6e4d7cb438ba7e4a9
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-10 23:08:59 -0300

    Home tests

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Carousel from "../Components/Carousel/Carousel"
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <h1 class="bg-#111827" >This is home</h1>
+      <Carousel/>
       <div >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates delectus asperiores voluptas consequuntur porro natus consequatur ratione facere nesciunt blanditiis iure tempora sit, incidunt ducimus nostrum maiores enim aliquid. Officia.
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, a expedita. Adipisci repudiandae maiores ipsum corporis provident repellendus, vel nisi ducimus magnam laudantium vitae error reiciendis? Odit, consequuntur quod! Incidunt!
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus porro nobis dolores nam cumque deleniti fuga et amet facilis delectus voluptas exercitationem esse ex officia, nesciunt veniam consequatur modi fugiat!
